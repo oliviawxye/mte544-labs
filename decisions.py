07e4465fd4d46cc6b1706874f832795ae1a86e38
Olivia Ye <oliviawxye@gmail.com>
2025-11-24 18:54:32 -0500

    point

--- decisions.py
+++ decisions.py
@@ -186,7 +186,7 @@ def main(args=None):
 
 if __name__=="__main__":
     argParser=argparse.ArgumentParser(description="point or trajectory") 
-    argParser.add_argument("--motion", type=str, default="trajectory")
+    argParser.add_argument("--motion", type=str, default="point")
     args = argParser.parse_args()
 
     main(args)
