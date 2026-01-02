6dd3388f2f1e8264c3167ba696a0b5e1dc60821c
olivia <77518590+oliviawxye@users.noreply.github.com>
2026-01-02 17:24:57 -0500

    Full-stack implementation (#14)

---
+++ planner.py
@@ -0,0 +1,83 @@
+
+import math
+from mapUtilities import mapManipulator
+from a_star import search
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+POINT_PLANNER=0; ASTAR_PLANNER=1
+
+PARABOLA=0; SIGMOID=1
+
+class planner:
+
+    def __init__(self, type_, mapName="room"):
+
+        self.type=type_
+        self.mapName=mapName
+        ## TODO: Adjust the laser_sig value which decides the safety distance to obstacles
+        self.m_utilites = mapManipulator(filename_=self.mapName, laser_sig=0.5)
+        self.costMap = self.m_utilites.make_likelihood_field()
+
+    
+    def plan(self, startPose, endPose):
+        
+        if self.type==POINT_PLANNER:
+            return self.point_planner(endPose)
+        
+        
+        elif self.type==ASTAR_PLANNER:
+            print("planning a star")
+            return self.trajectory_planner(startPose, endPose)
+        
+        else:
+            print("Error! you don't have this type of planner")
+            return None
+        
+
+    def point_planner(self, endPose):
+        return (endPose[0], endPose[1])
+
+
+    def trajectory_planner(self, startPoseCart, endPoseCart):
+        start_time = time.time()
+        startPoseCart = np.array(startPoseCart)[:2]
+        endPoseCart = np.array(endPoseCart)[:2]
+
+        # TODO: Convert to pixel coordinates using the m_utilites
+        startPose = self.m_utilites.position_2_cell(startPoseCart)
+        endPose = self.m_utilites.position_2_cell(endPoseCart)
+
+        # convert to tuple
+        startPose = (startPose[0], startPose[1])
+        endPose = (endPose[0], endPose[1])
+        # TODO: Call the A* search algorithm
+        path = search(self.costMap, startPose, endPose)
+        if path is None:
+            print("no path?")
+            return None
+        
+        pathCart = self.m_utilites.cell_2_position(path)
+        pathCart_list = pathCart.tolist()
+        print("Time taken for A* is ", time.time()-start_time)
+
+        # Visualization
+        allObstacles = np.array(self.m_utilites.getAllObstacles())
+        plt.plot(allObstacles[:, 0], allObstacles[:, 1], 'ko', markersize=4)
+        plt.axis('equal')
+        plt.title('A* path planning')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        # plot the path
+        plt.plot(pathCart[:, 0], pathCart[:, 1], 'b-', linewidth=3)
+        # plot the start and end points
+        plt.plot(startPoseCart[0], startPoseCart[1], 'g*', markersize=10)
+        plt.plot(endPoseCart[0], endPoseCart[1], 'r*', markersize=10)
+        plt.text(startPoseCart[0], startPoseCart[1], 'start', fontsize=16)
+        plt.text(endPoseCart[0], endPoseCart[1], 'goal', fontsize=16)
+        plt.show()
+
+        return pathCart_list
+        
+    
---
+++ rubrics.md
@@ -0,0 +1,32 @@
+# Rubric for Lab 4: In-lab Evaluation
+As suggested by Professor Jeon, there is **no report required for Lab 4**. 
+However, to ensure a comprehensive evaluation of your understanding and implementation of the A star planning and all the labs, we have designed the following rubric for grading your lab work.
+
+## Pre-lab Submission: 10 marks
+- ```10``` marks for submitting a first version of the completed code before the group's lab section.
+
+## Part 3: Map Acquisition and Saving: 10 marks
+- ```10``` marks for successful map acquisition and saving as `room` map.
+
+## Part 4 & 5: Understanding of A Star Planning and the Whole Autonomous Mobile Robot System: 40 marks
+- ```20``` marks for explaining the A star planning algorithm to a TA.
+    - Explain the following components:
+        - Two Heuristic functions (h Manhattan and Euclidean).
+        - Cost functions (g, h, f).
+        - How to select the next node to expand.
+    - Why A star is more efficient than Dijkstra's algorithm.
+- ```20``` marks for explaining the integration of all components in the autonomous mobile robot system to a TA.
+    - What components you have implemented in this lab series (L1 to L4).
+    - The role of each component in the overall system.
+    - How these components work together to enable autonomous navigation.
+
+
+## Part 6: Robot Navigation using A Star Planning: 40 marks
+- ```10``` marks for successful implementation of A star planning for robot navigation (showing planned path on RViz).
+- ```30``` marks for the robot successfully navigating from start to goal positions without collisions (3 experiments will be conducted).
+    - Two goal positions using one heuristic function.
+    - One goal position using the other heuristic function.
+# Total: 100 marks
+
+Good luck!
+
